94c68393181d7dfabc1d35d07e281a68074e2687
kirill <crownbarrel@gmail.com>
2025-02-11 21:11:48 +0500

    dependency injection

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -1,4 +1,56 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/watchlist-kata/auth/cmd/config"
+	crl "github.com/watchlist-kata/auth/internal/controller/grpc"
+	pb "github.com/watchlist-kata/auth/internal/controller/grpc/proto"
+	repo "github.com/watchlist-kata/auth/internal/repository/postgres"
+	sv "github.com/watchlist-kata/auth/internal/service"
+	"github.com/watchlist-kata/auth/pkg/tokenJWT"
+
+	"google.golang.org/grpc"
+)
+
 func main() {
+	cf := config.LoadConfig()
+	jwt := tokenJWT.NewTokenJWT(cf.SecretKey, time.Minute*15)
+
+	db := config.NewPostgres().InitDB(cf)
+	defer db.Close()
+
+	serverGRPC := grpc.NewServer()
+
+	pb.RegisterAuthServiceServer(serverGRPC,
+		crl.NewAuthServiceGRPC(sv.NewAuthSeviceImpl(jwt,
+			repo.NewPostgesRepositoryImpl(db.GetDB()))))
+
+	l, err := net.Listen("tcp", fmt.Sprintf(":%v", cf.GRPCServerPort))
+	defer l.Close()
+
+	if err != nil {
+		log.Fatalf("listen error %w", err)
+	}
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		log.Println(fmt.Sprintf("The auth server gRPC is running on port %v", cf.GRPCServerPort))
+		if err = serverGRPC.Serve(l); err != nil {
+			log.Fatalf("Accept error %w", err)
+		}
+	}()
+
+	<-stop
+	serverGRPC.GracefulStop()
+	log.Println("Server stopped")
+
 }
--- go.mod
+++ go.mod
@@ -9,10 +9,16 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 	golang.org/x/crypto v0.32.0
+	google.golang.org/grpc v1.64.1
+	google.golang.org/protobuf v1.34.2
 )
 
 require (
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240513163218-0867130af1f8 // indirect
 )
